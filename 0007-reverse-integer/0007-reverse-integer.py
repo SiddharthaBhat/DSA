5fca00502de728b6318d3024978e21bda1b9f8e6
SiddharthaBhat <siddusbs2810@gmail.com>
2026-09-22 20:34:24 +0530

    Time: 16 ms (69.69%), Space: 12.4 MB (14.73%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+
+        rev = 0
+
+        while x > 0:
+            digit = x % 10
+            x //= 10
+
+            if rev > 214748364 or (rev == 214748364 and digit > 7):
+                return 0
+
+            rev = rev * 10 + digit
+
+        return sign * rev
+        
